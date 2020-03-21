ce049b300a1c8d62880131b582c57abed66b975f
lalitcrane <48549060+lalitcrane@users.noreply.github.com>
2020-03-21 21:52:07 +0530

    Update QuickNumpy.py

--- QuickNumpy.py
+++ QuickNumpy.py
@@ -6,3 +6,25 @@ a.mean()
 np.median(a)
 np.std(a)
 
+a.reshape(x,y)
+a.flatten() # Returns flattenend one dimentional array
+a.ravel() # same as flatten, but flattens original array
+a.transpose()
+a.swapaxes()
+a.flat - Not a function, jut an iterator for flattened version of array
+
+
+#Joining
+np.concatenate(x,y)
+np.stack(a,b)
+np.hstack(a,b)
+np.vstack(a,b)
+
+np.append(a,[...])
+
+# Saving and Reading Files
+np.save('myfile.npy', a) # Save in Numpy Format
+#CSV Format
+np.savetxt('myfile.txt', a)
+a = np.loadtxt('myfile.txt')
+a = np.loadtxt('myfile.csv', delimiter=';')
