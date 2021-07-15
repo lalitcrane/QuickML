c883545ae1b1e4db19e09e05a2e8d615a2751eac
lalitcrane <48549060+lalitcrane@users.noreply.github.com>
2021-07-15 12:43:09 +0530

    Create ML_ModelsAndPackages.py

---
+++ ML_ModelsAndPackages.py
@@ -0,0 +1,18 @@
+
+
+Timeseries package
+
+Kats package from facebook research
+    https://facebookresearch.github.io/Kats/
+    (forecasting, detectection, trends, MLModels, many other utilities)
+  * Models available 
+    Linear
+    Quadratic
+    ARIMA
+    SARIMA
+    Holt-Winters
+    Prophet
+    AR-Net
+    LSTM
+    Theta
+    VAR
